7cb7b735c5554383d5a5d62a15653165604dbd9e
Aman5613 <161672923+Aman5613@users.noreply.github.com>
2025-04-21 18:09:50 +0530

    first commit

---
+++ scipt.js
@@ -0,0 +1,12 @@
+
+document.getElementById('blue-btn').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'blue';
+});
+
+document.getElementById('red-btn').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'red';
+});
+
+document.getElementById('green-btn').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'green';
+});
